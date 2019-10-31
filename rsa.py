fc0b903ab2513b0a15606e5fa91e87a21a00d6a6
Kragzmo-Simon <fery.simon@ymail.com>
2019-10-31 22:06:06 +0100

    Changed number of trials of miller rabin test

--- rsa.py
+++ rsa.py
@@ -5,13 +5,17 @@ print("Code par FERY Simon et DURAFFOURG Maud")
 print('Exercice RSA')
 
 
+# message à chiffrer et déchiffrer
+m = 65483
+
 # Générer un nombre avec un certain nombre de chiffres
 #print(len(str(2**2048)))
-nb_digits = 200
+nb_digits = 2
 
 # Générer p et q
 p = generate_prime_number(nb_digits)
 q = generate_prime_number(nb_digits)
+# TODO : verifier distinction entre p et q
 print("p choisi : ", p, "\n")
 print("q choisi : ", q, "\n")
 
@@ -21,7 +25,8 @@ print("n calculé : ", n, "\n")
 
 
 # Choisir un exposant de chiffrement e
-e = 0
+e = 9007
+print(is_Prime(e))
 
 
 
